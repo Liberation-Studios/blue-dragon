ef19a028880623eb673b33ef3f575f40a574ac01
TheBot99 <vinaymaryala@outlook.com>
2023-11-20 18:27:59 -0600

    created functions to cnvert what is a 1d array virtually to 2d

--- tileEngine/tileEngine.go
+++ tileEngine/tileEngine.go
@@ -1,17 +1,37 @@
 package tileEngine
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func BigTileArrayInput() {
 	const tileChunkSize uint8 = 4
-	tileChunk := [tileChunkSize * tileChunkSize]int{0}
+	tileChunk := [16]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 720, 11, 12, 13, 14, 15}
 
-	fmt.Println(tileChunk)
+	TranslateCoordinates(tileChunkSize)
+	fmt.Println(GetTile(tileChunk, 2, 2, tileChunkSize))
 }
 
-func TranslateCoordinates() {
-	var x = 4
-	var y = 2
-	var tileChunkSize = 4
+func TranslateCoordinates(tileChunkSize uint8) {
+	var x uint8 = 2
+	var y uint8 = 2
+	var width uint8 = 4 // Width of the grid
+
+	if x < 0 || x >= width || y < 0 || y >= width {
+		fmt.Println("Coordinates are out of bounds")
+		return
+	}
+
+	index := convertTo1DIndex(x, y, width)
+	fmt.Println("1D Index:", index)
+
+}
+
+func convertTo1DIndex(x, y, columns uint8) uint8 {
+	return x + y*columns
+}
+
+func GetTile(tileChunk [16]int, x, y, columns uint8) int {
+	return tileChunk[convertTo1DIndex(x, y, columns)]
 
 }
